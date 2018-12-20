63d1ea14527a2f514be13525e688a96ffef6ec29
denis <denis@newrow.com>
2018-12-20 11:52:55 +0200

    Added recaptcha.net support

--- src/recaptcha-wrapper.js
+++ src/recaptcha-wrapper.js
@@ -5,7 +5,11 @@ const callbackName = "onloadcallback";
 const lang = typeof window !== "undefined" && (window.recaptchaOptions && window.recaptchaOptions.lang) ?
   `&hl=${window.recaptchaOptions.lang}` :
 	"";
-const URL = `https://www.google.com/recaptcha/api.js?onload=${callbackName}&render=explicit${lang}`;
+const hostName = typeof window !== "undefined" && (window.recaptchaOptions && window.recaptchaOptions.useRecaptchaNet) ?
+    "recaptcha.net" :
+    "www.google.com";
+
+const URL = `https://${hostName}/recaptcha/api.js?onload=${callbackName}&render=explicit${lang}`;
 const globalName = "grecaptcha";
 
 export default makeAsyncScriptLoader(ReCAPTCHA, URL, {
